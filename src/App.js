20ac6b399b277ac110f07bc59c94d3a37e82bdfc
bini219 <binyamasrat@gmail.com>
2024-03-06 12:52:42 +0300

    make master for new start

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
-
 function App() {
-  const expenses = [
-    {
-      id: "1",
-      title: "chekes",
-      date: new Date(),
-      amount: 70.21,
-    },
-    {
-      id: "2",
-      title: "Alkol",
-      date: new Date(),
-      amount: 7.21,
-    },
-    {
-      id: "3",
-      title: "Bill",
-      date: new Date(),
-      amount: 500.21,
-    },
-  ];
-
   return (
     <div>
-      <h1>Hello Cellare</h1>
-      <Expenses expenses={expenses} />
+      Empty App
     </div>
   );
 }
--- src/components/UI/Card.js
+++
@@ -1,7 +0,0 @@
-import "./Card.css";
-
-const Card = (props) => {
-  const classNames = "card " + props.className;
-  return <div className={classNames}>{props.children}</div>;
-};
-export default Card;
